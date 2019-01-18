87c317300b5d1f9cbfd8388c387bbacd6bceeef3
Dejan Stjepanovic <ds2q88@gmail.com>
2019-01-17 23:25:05 -0600

    most of my sql to delete

---
+++ updateRecap.sql
@@ -0,0 +1,55 @@
+/*
+select * 
+from Surveys 
+where SurveyId = 14
+update Surveys set Name = 'Do you like this app?'*/
+
+/*select * from Surveys
+update Surveys set Name = 'That DQ blizzard'
+where SurveyId = 10 */
+
+/*select * from Questions
+update Questions set Content =  'Would you use this app?'
+where QuestionId = 36 */
+
+/*select * from AspNetUsers */
+
+--select * from AnswersSurveyInstances
+--delete AnswersSurveyInstances
+--where AnswerSurveyInstanceId = 184
+
+--select * from SurveyInstances
+--delete SurveyInstances
+--where SurveyInstanceId = 53
+
+--select * from SurveyInstances
+--delete SurveyInstances
+--where SurveyInstanceId = 54
+
+--select * from SurveyInstances
+--delete SurveyInstances
+--where SurveyInstanceId = 55
+
+--select * from SurveyInstances
+--delete SurveyInstances
+--where SurveyInstanceId = 56
+
+--select * from SurveyInstances
+--delete SurveyInstances
+--where SurveyInstanceId = 59
+
+--select * from Answers
+--delete Answers
+--where QuestionId = 36
+
+--select * from Questions
+--delete Questions
+--where QuestionId = 36
+
+--select * from Surveys
+--delete Surveys
+--where SurveyId = 14
+
+select * from AspNetUsers
+delete AspNetUsers
+where UserName = 'Gil69@gmail.com'
